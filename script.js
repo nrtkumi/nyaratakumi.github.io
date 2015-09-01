049044159d6830b444523ab712834c0cecb142f7
奈良拓海 <takuminara0428@gmail.com>
2015-09-01 20:55:29 +0900

    type

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(function() {
 
 $('#particles').particleground({
-    dotColor: '#bcbcbc', //ドットの色
-    lineColor: '#bcbcbc', //線の色
-    particleRadius: 5 //ドットの大きさ
-});
+    dotColor: '#00ffba', //ドットの色
+    lineColor: '#00ff12', //線の色
+    particleRadius: 5, //ドットの大きさ
+    minSpeedX: 0.1,
+    density: 7000,
+    parallax: false
+  });
 
 });

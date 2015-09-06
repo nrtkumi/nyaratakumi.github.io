61f9530250a8fc990feada28712605478a04a3db
奈良拓海 <takuminara0428@gmail.com>
2015-09-06 13:58:57 +0900

    ver 0.9

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
 
-$('#particles').particleground({
+  $('#particles').particleground({
     dotColor: '#00ffba', //ドットの色
     lineColor: '#00ff12', //線の色
     particleRadius: 5, //ドットの大きさ
@@ -9,4 +9,48 @@ $('#particles').particleground({
     parallax: false
   });
 
+  $('a[href^=#about]').click(function() {
+      var speed = 400;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+
+  $('a[href^=#history]').click(function() {
+      var speed = 400;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+
+   $('a[href^=#skill]').click(function() {
+       var speed = 400;
+       var href= $(this).attr("href");
+       var target = $(href == "#" || href == "" ? 'html' : href);
+       var position = target.offset().top;
+       $('body,html').animate({scrollTop:position}, speed, 'swing');
+       return false;
+    });
+
+    $('a[href^=#made]').click(function() {
+        var speed = 400;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+     });
+
+     $('a[href^=#contact]').click(function() {
+         var speed = 400;
+         var href= $(this).attr("href");
+         var target = $(href == "#" || href == "" ? 'html' : href);
+         var position = target.offset().top;
+         $('body,html').animate({scrollTop:position}, speed, 'swing');
+         return false;
+      });
 });

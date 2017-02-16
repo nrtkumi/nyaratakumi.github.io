f9ac235461857233a8f9dbe87f06e773fdb12710
TakumiNara <takuminara0428@gmail.com>
2017-02-16 17:07:10 +0900

    add text

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(window).scroll(function() {
 $('.content').click(function() {
   $('#modal-overlay').toggle();
   $('.modal-content').css("display", "flex");
-  $('.here').append('<p>something</p>');
+  showText($('.content').index(this));
 });
 
 $('#modal-close').click(function() {
@@ -25,3 +25,13 @@ $('#modal-close').click(function() {
   $('.modal-content').css("display", "none");
   $('.here').html('');
 });
+
+function showText(index) {
+  if (index === 0) {
+    $('.here').append('<p>奈良拓海</p><p>1998年4月28日生まれ</p><p>すごいサービス作ってます</p><p>仕事をください</p>');
+  } else if (index === 1) {
+    $('.here').append('<p>できる順</p><p>Ruby on Rails(Ruby)</p><p>Javascript(Node.js, Vue.js)</p><p>HTML/CSS</p><p>Android(Java)</p><p>openFrameworks(C++)</p><p>English</p>');
+  } else if (index === 2) {
+    $('.here').append('<p>2016.12.17<br>中高生No.1を決めるクックパッドハッカソン 最優秀賞</p>');
+  }
+}
